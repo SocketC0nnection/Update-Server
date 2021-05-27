d1c5cee045adad70ccbce0c0edc371330fa51380
SocketC0nnection <58369385+SocketC0nnection@users.noreply.github.com>
2021-05-27 15:49:36 +0200

    Performance Update

--- src/main/java/net/autoupdater/src/client/Client.java
+++ src/main/java/net/autoupdater/src/client/Client.java
@@ -16,55 +16,42 @@ public class Client implements Runnable {
     private final AutoUpdater instance = AutoUpdater.getInstance();
 
     private final Socket socket;
-    private final BufferedReader reader;
-    private final OutputStream outputStream;
+    private final DataInputStream inputStream;
+    private final DataOutputStream outputStream;
 
     public Client(Socket client) throws IOException {
-        this.socket = new Socket();
-        this.reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
-        this.outputStream = client.getOutputStream();
+        socket = new Socket();
+        inputStream = new DataInputStream(client.getInputStream());
+        outputStream = new DataOutputStream(client.getOutputStream());
     }
 
     @Override
     public void run() {
         try {
-            String msg = reader.readLine();
+            double version = inputStream.readDouble();
 
-            if(msg == null || !msg.contains(" ")) {
-                return;
-            }
+            Pair<File, Double> latestVersion = instance.getVersionManager().getLatestVersion();
 
-            String[] args = msg.split(" ");
-
-            if(args.length != 2 || !args[0].equals("VERSION")) {
-                return;
-            }
-
-            Pair<File, Double> version = instance.getVersionManager().getLatestVersion();
-            PrintWriter writer = new PrintWriter(outputStream);
-
-            if(version == null || version.getValue() <= Double.parseDouble(args[1])) {
-                writer.println("VERSION LATEST");
-                writer.flush();
+            if(latestVersion == null || latestVersion.getValue() <= version) {
+                outputStream.writeDouble(version);
+                outputStream.flush();
 
                 return;
             }
 
-            writer.println("VERSION " + version.getValue());
-            writer.flush();
+            outputStream.writeDouble(latestVersion.getValue());
+            outputStream.flush();
 
-            File file = version.getKey();
-
+            File file = latestVersion.getKey();
             FileInputStream fileIn = new FileInputStream(file);
-            DataOutputStream dataOut = new DataOutputStream(outputStream);
 
             byte[] buffer = new byte[(int) file.length()];
 
             fileIn.read(buffer);
 
-            dataOut.writeInt(buffer.length);
-            dataOut.write(buffer);
-            dataOut.flush();
+            outputStream.writeInt(buffer.length);
+            outputStream.write(buffer);
+            outputStream.flush();
 
             socket.close();
         } catch (Exception e) {
